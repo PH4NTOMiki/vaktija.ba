c13d23a6a780bd9e913c0ecf3cc6e403a94e6b4c
Armin Causevic <armin61@gmail.com>
2026-01-27 11:48:28 +0100

    Took 24 seconds

--- src/components/CurrentDate.js
+++ src/components/CurrentDate.js
@@ -7,7 +7,7 @@ function CurrentDate({ date, theme, location, locations }) {
       {date[0]}{" "}
       <a
         className={`year year-${theme}`}
-        href={`/download/pdf/${locations[location]}_${date[1]}.pdf`}
+        href={`https://www.islamskazajednica.ba/images/download/takvim_${date[1]}.pdf`}
       >
         {date[1]}
       </a>{" "}
